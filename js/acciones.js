bbc64781ef3510df59017f0962b08639b76063ea
ar2403 <ar2403_1@hotmail.com>
2015-06-12 21:04:15 -0500

    2
    a

--- js/acciones.js
+++ js/acciones.js
@@ -6,7 +6,7 @@ function BuscarPorNombre(Quien)
 	 = "Evento="+Quien;
 	$.ajax({
 		type: "POST",
-		url: "http://192.168.1.182/EventosINAOE/consultanombre_evento.php",
+		url: "http://192.168.1.75/EventosINAOE/consultanombre_evento.php",
 		data: datos
 	}).done(function(msg){
 		alert(msg);
@@ -38,7 +38,7 @@ function BuscarPorParticipante(Quien)
 	datos = "nombre_participante="+Quien;
 	$.ajax({
 		type: "POST",
-		url: "http://192.168.1.182/EventosINAOE/consultanombre_participante.php",
+		url: "http://192.168.1.75/EventosINAOE/consultanombre_participante.php",
 		data: datos
 	}).done(function(msg){
 		alert(msg);
@@ -63,36 +63,6 @@ if (DatosJSON.datos==0)
 	});
 }
 
-function Listado()
-{
-	
-	$.ajax({
-		type: "POST",
-		url: "http://192.168.1.182/catalogopeliculas/consultalistado.php",
-		
-	}).done(function(msg){
-		alert(msg);
-		var DatosJSON=JSON.parse(msg);
-		if (DatosJSON.datos==1)
-		{
-			
-			$('#Tabla').empty();
-			for (var i=0; i<DatosJSON.peli.length;i++)
-			{
-				
-			  $('#Tabla').append('<div align="justify" style="clear:both"><div style="float:left; width:50%"><h4>Id Pelicula:</h4><div id="RIdPelicula">'+DatosJSON.peli[i].Id+'</div><h4>Nombre:</h4><div id="RNombre">'+DatosJSON.peli[i].Nombre+'</div><h4>Director:</h4><div id="RDirector">'+DatosJSON.peli[i].Director+'</div><h4>Genero:</h4><div id="RGenero">'+DatosJSON.peli[i].Genero+'</div><h4>Duracion:</h4><div id="RDuracion">'+DatosJSON.peli[i].Duracion+'minutos</div></div><div style="float:left; width:50%"><img src="http://192.168.1.177/catalogopeliculas/recursos/fotos/'+DatosJSON.peli[i].Id+'.jpg" class="FotosP"></div><strong><hr></strong></div>');	
-			}
-			$('.FotosP').width($('#Tabla').width()*0.5);
-			$('#Nom').trigger('pagecreate');
-		}
-if (DatosJSON.datos==0)
-		{
-			alert('No hay Peliculas que Mostrar');
-		}		
-		
-		
-	});
-}
 
 
 
@@ -109,11 +79,7 @@ $(document).ready(function(e) {
     BuscarPorParticipante($('#buscarP').val());
   });
   
-  $('#ListadoP').tap(function(){
-	 
-    Listado();
-	$.mobile.changePage('#Lis');
-  });
+ 
   
   
  },false); //deviceready
